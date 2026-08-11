a62b886775a8bdab277039b502655516dd9728d2
gbrielbaiao <jarviplacanal@gmail.com>
2026-08-11 14:55:36 -0400

    Mudando a forma de acesso para evitar problemas de sessão.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Um sistema que gera um relatório de notas do SUAP IFMT.
 ## Dependencias
  - dotenv   -> `pip install python-dotenv`;
  - requests -> `pip install requests`
+ - selectolax -> `pip install selectolax==0.4.10`
--- main.py
+++ main.py
@@ -1,29 +1,55 @@
 from dotenv import load_dotenv
 from os import getenv
 import requests
-import json
-
-from functions.cookies import carregarCookies, salvarCookies
 
 load_dotenv()
 
-
 def fazer_login():
     url_login = "https://suap.ifmt.edu.br/accounts/login/?next=/"
-    EMAIL = getenv("EMAIL")
+    MATRICULA = getenv("MATRICULA")
     SENHA = getenv("SENHA")
 
-    parametros = {
-        "username": EMAIL,
-        "password": SENHA
+    session = requests.Session()
+
+    session.get(url_login)
+    csrftoken = session.cookies.get("__Host-csrftoken")
+
+    if not csrftoken:
+        print("Não consegui obter o csrftoken.")
+        return None
+
+    payload = {
+        "username": MATRICULA,
+        "password": SENHA,
+        "csrfmiddlewaretoken": csrftoken,
     }
+    headers = {
+        "Referer": url_login,
+    }
+    session.post(url_login, data=payload, headers=headers)
 
-    return requests.get(url_login, parametros)
+    print("Login realizado com sucesso!")
+    return session
 
+def acessarBoletim(url, session) -> str | None:
+    res = session.get(url)
+    if not res: return None
+    if res.status_code != 200: return None
+    return res.content.decode()
 
-cookies = carregarCookies()
-if cookies:
-    print(cookies)
-else:
-    cookies = fazer_login()
-    salvarCookies(cookies)
+def main():
+    session = fazer_login()
+
+    periodos = ["2024_1", "2025_1", "2026_1"]
+    for periodo in periodos:
+        url_boletim = f"https://suap.ifmt.edu.br/edu/aluno/{getenv("MATRICULA")}/?tab=boletim&ano_periodo={periodo}"
+        paginaBoletim = session.get(url_boletim).content.decode()
+
+        if not paginaBoletim:   
+            return
+
+        with open(f'paginaboletin-{periodo}.html', 'w', encoding="utf-8") as arquivo:
+            arquivo.write(paginaBoletim)
+        arquivo.close()
+
+main()
